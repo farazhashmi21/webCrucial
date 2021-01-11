c2e79301b4aade9286fd7894466cd2bdce7abe25
farazhashmi21 <developerfaraz@yahoo.com>
2021-01-11 15:00:11 +0500

    Project Create and Update UpTo Latest

--- project.sh
+++ project.sh
@@ -1,6 +1,6 @@
-echo "# webCrucial";
+echo "webCrucial";
 echo $(git add .);
 echo $(git commit -m "Project Create and Update UpTo Latest");
 echo $(git branch -M main);
-echo $(git remote add origin https://github.com/farazhashmi21/webCrucial.git);
+echo $(git remote -v);
 echo $(git push -u origin main);
